0009b78734cd32907dbb1d534db0ee76e6895071
hmj2422026 <hmj2422026@stu.o-hara.ac.jp>
2025-06-16 15:10:30 +0900

    アニメーション作り直し

--- Assets/Scripts/ReasureChest.cs
+++ Assets/Scripts/ReasureChest.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
-using System.Collections; // コルーチン（IEnumerator）を使うために必要
 
 public class ReasureChest : MonoBehaviour
 {
-	public Animator targetAnimator; // アニメーション対象
-	public string boolParameterName = "IsOpen"; // Animatorのboolパラメータ名
+	public Animator animator;
+	public string boolParameter = "IsOpen";
 
-	private void OnTriggerEnter(Collider other)
+	void Update()
 	{
-		if (other.CompareTag("Player"))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			targetAnimator.SetBool(boolParameterName, true);
+			animator.SetBool(boolParameter, true);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			animator.SetBool(boolParameter, false);
 		}
 	}
 }
